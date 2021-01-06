f952300daaa325ba4765408538f288bf2d56657f
VEnghed <enghed@gmail.com>
2021-01-06 15:15:53 +0100

    added some

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -1,38 +1,38 @@
-console.log('hello, client.')
-
 var socket
+var prevX = -1
+var prevY = -1
 
 function setup() {
-  
+
   socket = io.connect('http://localhost:1337')
 
-  socket.on('hey', () => console.log('connected'))
-  
   let canvas = createCanvas(600, 400);
   canvas.parent('sketch-wrapper')
   background(0)
   noStroke()
 
-  
-  
-  var button1 = document.getElementById('b1')
-  button1.onclick = send
+  socket.on('stroke', data => {
+    ellipse(data.x, data.y, 15, 15)
+  })
 
-
+  
 }
 
 function draw() {
   let x = Math.floor(mouseX)
   let y = Math.floor(mouseY)
 
-  if (mouseIsPressed) {
-    ellipse(x, y, 15, 15)
-    send(x, y)
-  }
+  if (mouseIsPressed && x >= 0 && y >= 0 && x <= 600 && y <= 400)
+    if (prevX != x && prevY != y) {
+      prevX = x
+      prevY = y
+      ellipse(x, y, 15, 15)
+      send(x, y)
+    }
 }
 
 function send(x, y) {
-  socket.emit('message', {x: x, y: y})
+  socket.emit('message', { x: x, y: y })
   console.log('sending')
 }
 
--- server.js
+++ server.js
@@ -3,35 +3,33 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const session = require('express-session')
 var FileStore = require('session-file-store')(session)
-const io = require('socket.io')
+const io = require('socket.io');
+const sharedsession = require('express-socket.io-session')
+
 
 const app = express();
+
+var sessions = {}
+
+var sessconf = session({
+    store: new FileStore({}),
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true
+})
 
 // middleware
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
-app.use(session({
-    store: new FileStore({}),
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: true
-}))
+app.use(sessconf)
+
 
 // static content
 app.use(express.static("public"))
 
 
 app.get('/', (req, res) => {
-
-    if (req.session.number) {
-        req.session.number++
-        res.send('views: ' + req.session.number)
-    } else {
-        req.session.number = 1
-        res.send('yo :P')
-    }
-
-    //res.sendFile(__dirname + '/routes/welcome/index.html')
+    res.sendFile(__dirname + '/routes/welcome/index.html')
 })
 
 app.get('/game', (req, res) => {
@@ -41,11 +39,10 @@ app.get('/game', (req, res) => {
 app.post('/login', (req, res) => {
     console.log('(POST) Login: ' + req.body.name + ' into room ' + req.body.room + '.')
 
-    if (req.cookies.name) {
-        console.log('\t', req.cookies)
-    } else {
-        res.cookie('name', req.body.name)
-    }
+
+    console.log('setting the session name for this one')
+    req.session.name = req.body.name;
+    req.session.room = req.body.room;
 
     res.redirect('/game')
 })
@@ -54,9 +51,18 @@ let server = app.listen(1337, () => console.log('listening...'))
 
 var ws = io(server)
 
+ws.use((socket, next) => {
+    sessconf(socket.request, {}, next)
+})
+
 ws.on('connection', socket => {
-    //console.log(socket.handshake.headers.cookie)
-    //socket.emit('hey', {})
-    //socket.on('message', data => console.log(data))
-    //socket.on('disconnect', () => console.log('disconnected.'))
+
+    console.log('session from socket.io', socket.request.session.name)
+
+    socket.emit('init', {})
+    socket.on('message', data => console.log(data))
+
+    socket.on('disconnect', () => {
+        console.log(socket.request.session.name)
+    })
 })
