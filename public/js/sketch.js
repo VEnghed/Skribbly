762e29def1a0acb25cb4608d29a438e36cd2b8dc
HybridVenom <alexander.jonsson987@gmail.com>
2021-01-06 15:37:32 +0100

    removes player instead of room
    Co-Authored-By: Viktor Enghed <enghed@gmail.com>

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -1,39 +1,34 @@
-var socket
-var prevX = -1
-var prevY = -1
+var socket;
+var prevX = -1;
+var prevY = -1;
 
 function setup() {
+	socket = io.connect("http://localhost:1337");
 
-  socket = io.connect('http://localhost:1337')
+	let canvas = createCanvas(600, 400);
+	canvas.parent("sketch-wrapper");
+	background(0);
+	noStroke();
 
-  let canvas = createCanvas(600, 400);
-  canvas.parent('sketch-wrapper')
-  background(0)
-  noStroke()
-
-  socket.on('stroke', data => {
-    ellipse(data.x, data.y, 15, 15)
-  })
-
-  
+	socket.on("stroke", (data) => {
+		ellipse(data.x, data.y, 15, 15);
+	});
 }
 
 function draw() {
-  let x = Math.floor(mouseX)
-  let y = Math.floor(mouseY)
+	let x = Math.floor(mouseX);
+	let y = Math.floor(mouseY);
 
-  if (mouseIsPressed && x >= 0 && y >= 0 && x <= 600 && y <= 400)
-    if (prevX != x && prevY != y) {
-      prevX = x
-      prevY = y
-      ellipse(x, y, 15, 15)
-      send(x, y)
-    }
+	if (mouseIsPressed && x >= 0 && y >= 0 && x <= 600 && y <= 400)
+		if (prevX != x || prevY != y) {
+			prevX = x;
+			prevY = y;
+			ellipse(x, y, 15, 15);
+			send(x, y);
+		}
 }
 
 function send(x, y) {
-  socket.emit('message', { x: x, y: y })
-  console.log('sending')
+	socket.emit("message", { x: x, y: y });
+	console.log("sending");
 }
-
-
--- testserver.js
+++ testserver.js
@@ -1,75 +1,73 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-const session = require('express-session')
-const io = require('socket.io')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const io = require("socket.io");
 
 //dropping sessionstore as it interferes with websockets.
 
-const app = express()
+const app = express();
 
-let rooms = {}
+let rooms = {};
 
 const sessionMiddleware = session({
-    secret: 'keyboard-cat',
-    resave: true,
-    saveUninitialized: true
-})
+	secret: "keyboard-cat",
+	resave: true,
+	saveUninitialized: true,
+});
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cookieParser())
-app.use(sessionMiddleware)
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(sessionMiddleware);
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/routes/welcome/index.html')
-})
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/routes/welcome/index.html");
+});
 
-app.get('/game', (req, res) => {
+app.get("/game", (req, res) => {
+	//we should use ESJ here instead of serving a static file.
+	res.sendFile(__dirname + "/routes/game/index.html");
+});
 
-    //we should use ESJ here instead of serving a static file.
-    res.sendFile(__dirname + '/routes/game/index.html')
-})
+app.post("/login", (req, res) => {
+	req.session.name = req.body.name;
+	req.session.room = req.body.room;
+	console.log(req.session.name + " joined " + req.session.room);
+	res.redirect("/game");
+});
 
-app.post('/login', (req, res) => {
+let server = app.listen(1337, () => console.log("listening..."));
 
-    req.session.name = req.body.name;
-    req.session.room = req.body.room;
-    console.log(req.session.name + ' joined ' + req.session.room)
-    res.redirect('/game')
-})
-
-let server = app.listen(1337, () => console.log('listening...'))
-
-var ws = io(server)
+var ws = io(server);
 
 //my own middleware to expose the request, lets us access session data in ws
 ws.use((socket, next) => {
-    sessionMiddleware(socket.request, {}, next)
-})
+	sessionMiddleware(socket.request, {}, next);
+});
 
-ws.on('connection', socket => {
-    let name = socket.request.session.name
-    let room = socket.request.session.room
+ws.on("connection", (socket) => {
+	let name = socket.request.session.name;
+	let room = socket.request.session.room;
 
-    socket.join(room)
+	socket.join(room);
 
-    if (!rooms[room]){
-        console.log('Created room ' + room);
-        rooms[room] = {players: {}}
-    } 
-    rooms[room].players[name] = {points: 0}
+	if (!rooms[room]) {
+		console.log("Created room " + room);
+		rooms[room] = { players: {} };
+	}
+	rooms[room].players[name] = { points: 0 };
 
-    console.log(rooms)
-    
-    socket.on('message', data => {
-        socket.to(room).emit('stroke', data)
-    })
-    
-    socket.on('disconnect', () => {
-        console.log("bye, " + name)
-        delete rooms[room];
-        console.log(rooms)
-    })
-})
+	console.log(rooms);
+
+	socket.on("message", (data) => {
+		socket.to(room).emit("stroke", data);
+	});
+
+	socket.on("disconnect", () => {
+		console.log("bye, " + name);
+		delete rooms[room].players[name];
+		console.log(rooms);
+	});
+});
